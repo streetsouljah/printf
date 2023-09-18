f5227a84127e0c9ee628126091564f7d4ce422c8
PrincessChuks <princesschuks07@gmail.com>
2023-09-18 05:54:48 -0700

    this is the first commit for main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int _printf(const char *format, ...);
+
+#endif
